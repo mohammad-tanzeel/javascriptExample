a96e6f1cf4873ceba630153ff5368ea7fb11a492
Mohd Tanzeel <tanzeel.tech@gmail.com>
2022-07-28 23:47:28 +0530

    Promise All

--- PromiseAllExp.js
+++ PromiseAllExp.js
@@ -1,4 +1,10 @@
 
+p5 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 2000, 'p5 result');
+});
+
+// p5.then(value => console.log(value)).catch(err => console.log(err));
+
 	p1 = Promise.resolve(50);
 	p2 = 200
 	p3 = new Promise(function(resolve, reject) {
@@ -11,7 +17,7 @@
         }, 1000);
       });
 
-	Promise.all([p1, p2, p3, p4]).then(function(values) {
+	Promise.all([p1, p2, p3, p4, p5]).then(function(values) {
 		console.log(values)
 	}).catch(err => console.log("error found = "+err));
 
